29713bb5c711037c017357f4a9d3ed9603e9031c
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-10-10 20:12:42 -0400

    markers has been finished

--- src/app/maps/pages/markers/markers.component.ts
+++ src/app/maps/pages/markers/markers.component.ts
@@ -2,9 +2,33 @@ import { Component, ElementRef, AfterViewInit, OnDestroy, ViewChild } from '@ang
 
 import * as mapboxgl from 'mapbox-gl';
 
+interface Markers {
+  marker: mapboxgl.Marker,
+  color?: string,
+  center?: [number, number]
+}
+
 @Component({
   selector: 'app-markers',
-  templateUrl: './markers.component.html'
+  templateUrl: './markers.component.html',
+  styles: [`
+    .list-group {
+      position: fixed;
+      top: 20px;
+      right: 20px;
+      z-index: 99;
+    }
+    .message {
+      position: fixed;
+      bottom: 20px;
+      right: 20px;
+      z-index: 99;
+      color: red;
+    }
+    li {
+      cursor: pointer
+    }
+  `]
 })
 export class MarkersComponent implements AfterViewInit, OnDestroy {
   @ViewChild('divMap')
@@ -16,7 +40,7 @@ export class MarkersComponent implements AfterViewInit, OnDestroy {
 
   public center: [number, number] = [-71.132664, 8.620822];
 
-  constructor() { }
+  public markers: Markers[] = [];
 
   ngAfterViewInit(): void {
     this.map = new mapboxgl.Map({
@@ -26,6 +50,8 @@ export class MarkersComponent implements AfterViewInit, OnDestroy {
       zoom: this.zoomLevel,
       projection: {name: 'globe'}
     });
+
+    this.loadMarkers();
 
     const marketHTML: HTMLElement = document.createElement('em');
     marketHTML.classList.add('bi');
@@ -59,6 +85,70 @@ export class MarkersComponent implements AfterViewInit, OnDestroy {
     this.map.off('zoomend', () => {});
     this.map.off('move', () => {});
     this.map.off('style.load', () => {});
+    this.markers.forEach(marker => {
+      marker.marker.off('dragend');
+    });
+  }
+
+  private _createMarker(color: string, center: [number, number]): void {
+    const marker: mapboxgl.Marker = new mapboxgl.Marker({
+      draggable: true, color
+    })
+      .setLngLat(center)
+      .addTo(this.map);
+
+    marker.on('dragend', () => {
+      this.saveMarkers();
+    })
+
+    this.markers.push({marker, color});
+  }
+
+  public addMarker(): void {
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+
+    this._createMarker(color, this.center);
+
+    this.saveMarkers();
+  }
+
+  public goMarker(currentMarker: mapboxgl.Marker): void {
+    this.map.flyTo({
+      center: currentMarker.getLngLat()
+    })
+  }
+
+  public deleteMarker(i: number) {
+    const currentMarker: mapboxgl.Marker = this.markers[i].marker;
+    currentMarker.off('dragend');
+    currentMarker.remove();
+    this.markers.splice(i, 1);
+    this.saveMarkers();
+  }
+
+  public saveMarkers(): void {
+    const markersToSave = this.markers.map(marker =>  {
+      const {lng, lat} = marker.marker.getLngLat();
+      return {
+        color: marker.color,
+        center: [lng, lat]
+      }
+    });
+
+    localStorage.setItem('markers', JSON.stringify(markersToSave));
+  }
+
+  public loadMarkers(): void {
+    const markers: string | null = localStorage.getItem('markers');
+    if(!markers) {
+      return;
+    }
+
+    const markersSaved: Markers[] = JSON.parse(markers);
+
+    markersSaved.forEach(marker => {
+      this._createMarker(marker.color!, marker.center!);
+    });
   }
 
 }
